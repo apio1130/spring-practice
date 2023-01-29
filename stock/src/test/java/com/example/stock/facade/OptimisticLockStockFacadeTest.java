ccee0162896cb06ceb26864460ba75520b2d9327
kjkim <apio1130@naver.com>
2023-01-29 20:34:32 +0900

    feat: [재고시스템] Database 이용해보기 - Optimistic Lock
    Optimistic Lock 을 이용하여 레이스 컨디션 해결

---
+++ stock/src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
@@ -0,0 +1,65 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class OptimisticLockStockFacadeTest {
+
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @Autowired
+    private OptimisticLockStockFacade stockService;
+
+    @BeforeEach
+    void before() {
+        Stock stock = new Stock(1L, 100L);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        // 100 - 100 = 0
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0L, stock.getQuantity());
+    }
+}
